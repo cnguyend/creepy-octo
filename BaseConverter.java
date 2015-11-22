486161d61e34ea0ff4ec9938b5b0b09dba4219ee
Christian Nguyendinh <christiannguyendinh@yahoo.com>
2015-11-22 00:16:23 -0500

    First commit test

---
+++ BaseConverter.java
@@ -0,0 +1,13 @@
+
+public class BaseConverter {
+	int base;
+	int numberToConvert;
+	
+	public BaseConverter(int base, int numberToConvert){
+		this.base = base;
+		this.numberToConvert = numberToConvert;
+	}
+	
+	
+
+}
